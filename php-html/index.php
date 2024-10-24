9ffeba9901cd8cd66cae5bcbf244cdda4423ae75
rfjar <if23.rafifpradana@mhs.ubpkarawang.ac.id>
2024-10-24 13:12:12 +0700

    Add files via upload

---
+++ php-html/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>INI HALAMAN LOGIN</h1>
+    <form action="./aoa/login.php" method="POST">
+        <input type="email" name="email" id="email" placeholder="Email"/>
+        <input type="password" name="password" id="password" placeholder="Password"/>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
